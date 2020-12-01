d1ea069660f7ea88bbf2ddcc91b145bf236c3a0f
2dam3 <ikbem@plaiaundi.net>
2020-12-01 10:11:47 +0100

    terminado, pide nombre usuario y aparte manda mensaje broadcast y personalizado

--- src/com/company/Server/ConexionCliente.java
+++ src/com/company/Server/ConexionCliente.java
@@ -1,7 +1,6 @@
 package com.company.Server;
 
 import org.json.simple.JSONObject;
-
 import java.io.*;
 import java.net.Socket;
 import java.util.List;
@@ -12,9 +11,8 @@ public class ConexionCliente extends Thread{
     String nombre;
     BufferedWriter bw ;
 
-    ConexionCliente(Socket socket,String nombre){
+    ConexionCliente(Socket socket){
         this.socket = socket;
-        this.nombre = nombre;
         try{
             bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
         } catch (IOException exception) {
@@ -31,29 +29,47 @@ public class ConexionCliente extends Thread{
         } catch (IOException exception) {
             exception.printStackTrace();
         }
-
     }
 
     @Override
     public void run() {
+
         try{
+            while(this.nombre == null){
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                out.write("Inserte tu nombre de usuario: ");
+                out.newLine();
+                out.flush();
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                this.nombre = in.readLine();
+            }
+
             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
             String linea;
             while((linea = br.readLine()) != null){
-                String[] datos = linea.split("|");
-
+                String[] datos = linea.split("\\|");
                 JSONObject msg = new JSONObject();
-                    msg.put("from",this.nombre);
-                    msg.put("to",datos[0]);
-                    msg.put("ms",datos[1]);
+                    if(datos.length ==1) {
+                        msg.put("from",this.nombre);
+                        msg.put("to","all");
+                        msg.put("ms",datos[0]);
+                    }else if(datos[0] == "all"){
+                        msg.put("from",this.nombre);
+                        msg.put("to",datos[0]);
+                        msg.put("ms",datos[1]);
+                    }else{
+                        msg.put("from",this.nombre);
+                        msg.put("to",datos[0]);
+                        msg.put("ms",datos[1]);
+                    }
 
-                try (FileWriter file = new FileWriter("MSG.json")) {
-                    file.write(msg.toJSONString());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Server.broadcast(linea,this.nombre,socket);
+                BufferedWriter bw_json = new BufferedWriter(new FileWriter("MSG.json"));
+                bw_json.write(msg.toJSONString());
+                bw_json.close();
+
+                Server.controladorMSG(msg);
             }
         } catch (IOException exception) {
             exception.printStackTrace();
--- src/com/company/Server/Server.java
+++ src/com/company/Server/Server.java
@@ -1,5 +1,7 @@
 package com.company.Server;
 
+import org.json.simple.JSONObject;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -9,14 +11,26 @@ public class Server {
 
     private static ArrayList<ConexionCliente> clientesConectados = new ArrayList();
 
-    public static void broadcast(String s,String nombre,Socket socket){
-        for(ConexionCliente cliente : clientesConectados){
-            if(cliente.socket != socket) {
-                cliente.enviarMensaje(s,nombre);
+    public static void controladorMSG(JSONObject msg){
+        if(msg.get("to")=="all"){
+            for(ConexionCliente cliente : clientesConectados){
+                if(msg.get("from")!=cliente.nombre){
+                    if(cliente.nombre != null){
+                        cliente.enviarMensaje(msg.get("ms").toString(),"/all-"+msg.get("from").toString());
+                    }
+                }
+            }
+        }else{
+            for(ConexionCliente cliente : clientesConectados){
+                System.out.println("From:"+msg.get("from"));
+                System.out.println("To:"+msg.get("to"));
+                System.out.println("Msg:"+msg.get("ms"));
+                if(msg.get("from") !=cliente.nombre & msg.get("to").equals(cliente.nombre)){
+                    cliente.enviarMensaje(msg.get("ms").toString(),msg.get("from").toString());
+                }
             }
         }
     }
-
 
     public static void main(String[] args) {
 
@@ -30,15 +44,7 @@ public class Server {
                 Socket socket = servidor.accept();
                 System.out.println("Conexion realizada");
 
-                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-                out.write("Inserte tu nombre de usuario: ");
-                out.newLine();
-                out.flush();
-
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                String usuario = in.readLine();
-
-                ConexionCliente cc = new ConexionCliente(socket,usuario);
+                ConexionCliente cc = new ConexionCliente(socket);
                 cc.start();
                 clientesConectados.add(cc);
             }
