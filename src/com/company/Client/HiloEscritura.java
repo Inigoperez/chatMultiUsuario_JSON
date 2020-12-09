b5d8ea9664e0e72438f8f071c1222154c5e8d850
2dam3 <ikbem@plaiaundi.net>
2020-12-09 10:35:31 +0100

    cambiado codigo de hilo escritura, ahora envia jsonObject

--- src/com/company/Client/HiloEscritura.java
+++ src/com/company/Client/HiloEscritura.java
@@ -1,30 +1,57 @@
 package com.company.Client;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import org.json.simple.JSONObject;
+
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class HiloEscritura extends Thread{
     Socket socket;
-    BufferedWriter out ;
+    BufferedWriter outWrite ;
+    ObjectOutputStream outObj;
+    String nombre;
     Scanner sc = new Scanner(System.in);
 
     public HiloEscritura(Socket socket) throws IOException {
         this.socket = socket;
-        this.out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        this.outWrite = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        this.outObj = new ObjectOutputStream(socket.getOutputStream());
     }
 
     @Override
     public void run() {
         String linea;
-        while((linea = sc.nextLine()) != "terminar") {
+
+        while (nombre == null) {
+            linea = sc.nextLine();
             try {
-                out.write(linea);
-                out.newLine();
-                out.flush();
+                outWrite.write(linea);
+                outWrite.newLine();
+                outWrite.flush();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+
+        while ((linea = sc.nextLine()) != "terminar") {
+            String[] datos = linea.split("\\|");
+            JSONObject msg = new JSONObject();
+            if (datos.length == 1) {
+                msg.put("from", this.nombre);
+                msg.put("to", "all");
+                msg.put("ms", datos[0]);
+            } else if (datos[0] == "all") {
+                msg.put("from", this.nombre);
+                msg.put("to", datos[0]);
+                msg.put("ms", datos[1]);
+            } else {
+                msg.put("from", this.nombre);
+                msg.put("to", datos[0]);
+                msg.put("ms", datos[1]);
+            }
+            try {
+                outObj.writeObject(msg);
             } catch (IOException exception) {
                 exception.printStackTrace();
             }
